38757519ebdbd43dea707339f0ac5deca06a1257
Jonathon Harrelson <daygon07@gmail.com>
2023-06-09 02:02:51 -0500

    Updating styles and footer

--- components/footer.js
+++ components/footer.js
@@ -6,18 +6,18 @@ export default function Footer() {
         <>
             <div className="container">
                 <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                    <div className="col-md-4 d-flex align-items-center">
-                        <span className="mb-3 mb-md-0 text-body-secondary">© 2023 Unmasking Masculinity</span>
+                    <div className="col-md-5 d-flex align-items-center">
+                        <span className="mb-3 mb-md-0 text-body-secondary me-2">© 2023 Unmasking Masculinity</span> | <Link href="/privacy-policy/" className='me-2 ms-2'>Privacy Policy</Link> 
+                    | <Link href="/terms-of-use/" className='ms-2'>Terms of Use</Link>
                     </div>
 
                     <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-                        <li className="ms-3"><Link href="#" target='_blank'><i className='fa-brands fa-facebook fa-2x' /></Link></li>
-                        <li className="ms-3"><Link href="#" target='_blank'><i className='fa-brands fa-tiktok fa-2x' /></Link></li>
-                        <li className="ms-3"><Link href="#" target='_blank'><i className='fa-brands fa-linkedin fa-2x' /></Link></li>
-                        <li className="ms-3"><Link href="#" target='_blank'><i className='fa-brands fa-twitter fa-2x' /></Link></li>
-                        <li className="ms-3"><Link href="#" target='_blank'><i className='fa-brands fa-facebook fa-2x' /></Link></li>
-                        <li className="ms-3"><Link href="#" target='_blank'><i className='fa-brands fa-instagram fa-2x' /></Link></li>
-                        <li className="ms-3"><Link href="#" target='_blank'><i className='fa-brands fa-youtube fa-2x' /></Link></li>
+                        <li className="ms-3"><Link href="#" target='_blank' className='footer-social-link'><i className='fa-brands fa-facebook' /></Link></li>
+                        <li className="ms-3"><Link href="#" target='_blank' className='footer-social-link'><i className='fa-brands fa-tiktok' /></Link></li>
+                        <li className="ms-3"><Link href="#" target='_blank' className='footer-social-link'><i className='fa-brands fa-linkedin' /></Link></li>
+                        <li className="ms-3"><Link href="#" target='_blank' className='footer-social-link'><i className='fa-brands fa-twitter' /></Link></li>
+                        <li className="ms-3"><Link href="#" target='_blank' className='footer-social-link'><i className='fa-brands fa-instagram' /></Link></li>
+                        <li className="ms-3"><Link href="#" target='_blank' className='footer-social-link'><i className='fa-brands fa-youtube' /></Link></li>
                     </ul>
                 </footer>
             </div>
