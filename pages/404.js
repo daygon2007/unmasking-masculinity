f50db63a00fbd79dcd5784bbe8a46b8500d0337d
Jonathon Harrelson <daygon07@gmail.com>
2023-06-09 22:34:10 -0500

    Adding 404 page

---
+++ pages/404.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import Navigation from "@/components/nav";
+import Footer from "@/components/footer";
+import { getMenu } from "@/lib/menu";
+import Link from "next/link";
+
+export async function getStaticProps() {
+    const menu = await getMenu();
+
+    return {
+        props: {
+            menu: menu,
+        }
+    }
+}
+
+
+export default function NotFound({ menu }) {
+
+    return (
+        <>
+            <Head>
+                <title>No Found | Unmasking Masculinity</title>
+            </Head>
+            <Navigation menu={menu} />
+            <div id="not-found">
+                <div>
+                    <h1 className="white">Uh oh...</h1>
+                    <p className="lead white">What'cha doin here? This isn't what you're looking for.</p>
+                    <p className="white">Let's get you pointed in the right direction... Here's our <Link href="/podcast/">podcast</Link> and here's our <Link href="/blog/">blog</Link>. If there's anything specific you're looking for, <Link href="/contact/">contact us</Link>.</p>
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
